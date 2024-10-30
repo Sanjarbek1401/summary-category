9dfb87d54019720e8eb686317d2ec00348011ef9
Sanjarbek <sanjarbahodirov9901@gmail.com>
2024-10-30 23:42:12 +0500

    fix

--- audio_analysis/settings.py
+++ audio_analysis/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/root/summary-category/staticfiles/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR,'audio/static/')]
+STATIC_URL = '/root/summary-category/static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR,'audio/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = '/root/summary-category/media/'  # Fayl yuklash URL
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')

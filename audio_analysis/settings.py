533d13179c17b684cf1dc0000e640c47c34b716e
Sanjarbek <sanjarbahodirov9901@gmail.com>
2024-10-30 17:36:51 +0500

    nginx

--- audio_analysis/settings.py
+++ audio_analysis/settings.py
@@ -127,10 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/root/summary-category/static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'audio/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL = '/media/'  # Fayl yuklash URL
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
